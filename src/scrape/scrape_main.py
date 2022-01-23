54a123d1baf76be920812d0faabc07fe37210061
acseke <csekealpar12@gmail.com>
2022-01-23 11:59:53 +0100

    scrape batches combined

--- src/scrape/scrape_main.py
+++ src/scrape/scrape_main.py
@@ -1,5 +1,7 @@
+import pandas as pd
 from selenium import webdriver
 import os.path
+import glob
 
 from src.scrape.scrape_page_range import scrape_page_range
 
@@ -40,3 +42,19 @@ for year in nr_pages.keys():
             bac_results.to_csv(filepath)
 
 driver.close()
+
+
+###
+# consolidate scraped data
+
+results = []
+for year in nr_pages.keys():
+    year_files = glob.glob(folder + '/bac-results-' + year + '*.csv')
+    buffer = []
+    for filename in year_files:
+        batch = pd.read_csv(filename, index_col=None, header=0)
+        buffer.append(batch)
+    year_results = pd.concat(buffer, axis=0, ignore_index=True)
+
+    filepath = folder + 'bac-results-' + year + '.csv'
+    year_results.to_csv(filepath)
